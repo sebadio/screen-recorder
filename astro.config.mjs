5e517e744df062aad786511b990a31f92523e924
sebadio <sebastiandiazdopazo@gmail.com>
2024-01-09 19:29:02 -0300

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sebadio.github.io"
+  site: "https://sebadio.github.io",
   base: "/screen-recorder"
   integrations: [tailwind()],
 
